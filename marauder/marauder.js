d9e9d4979934842f1375b9e857b5053c5ad6e62b
Samira Bulle <sabulle@edu.uwaterloo.ca>
2018-03-12 23:39:11 -0400

    add google maps api

--- marauder/marauder.js
+++ marauder/marauder.js
@@ -1,21 +1,38 @@
-var subscribeKey = "sub-c-0cd4fdb4-2652-11e8-8f89-fe0057f68997";
-var publishKey = "pub-c-53ee49d7-c3d9-4787-b8f7-81d98202b5ff";
+var map;
+var subscribeKey;
+var publishKey;
+var pubnub;
 
-var pubnub = new PubNub({
-    subscribeKey: subscribeKey,
-    publishKey: publishKey,
-    ssl: true
-})
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 43.4723, lng: -80.5449},
+		zoom: 4
+	});
+	console.log("Map is drawn.");
+}
 
-pubnub.addListener({
-	status: function(statusEvent) {
-        console.log(statusEvent.category);
-    },
-    message: function(message) {
-    	console.log("Message:", message);
-    }
-})
+var main = function() {
+	subscribeKey = "sub-c-0cd4fdb4-2652-11e8-8f89-fe0057f68997";
+	publishKey = "pub-c-53ee49d7-c3d9-4787-b8f7-81d98202b5ff";
 
-pubnub.subscribe({
-    channels: ['LOCATION'],
-});
+	pubnub = new PubNub({
+	    subscribeKey: subscribeKey,
+	    publishKey: publishKey,
+	    ssl: true
+	});
+
+	pubnub.addListener({
+		status: function(statusEvent) {
+	        console.log(statusEvent.category);
+	    },
+	    message: function(message) {
+	    	console.log("Message:", message);
+	    }
+	});
+
+	pubnub.subscribe({
+	    channels: ['LOCATION'],
+	});
+}
+
+$(document).ready(main);
